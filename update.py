fe6642fd11a77fb75a07140d55359904bd0b58e2
az3r0th <saurabhthe1000@gmail.com>
2020-01-29 03:39:02 -0900

    firefox updater

---
+++ update.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import os
+url = "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
+r =requests.get(url)
+with open("update.tar.gz","wb+") as f:
+    f.write(r.content)
+os.system("sudo tar -xvf update.tar.gz -C /opt")
+os.system("rm update.tar.gz")
